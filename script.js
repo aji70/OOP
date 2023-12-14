f8ec03c31042071a9212249bcdad192c37727160
aji70 <113303791+aji70@users.noreply.github.com>
2023-12-14 22:24:07 +0100

    OOP

--- script.js
+++ script.js
@@ -8,5 +8,22 @@ Person.prototype.calcAge = function(){
     console.log(2023 - this.birthYear);
 }
 
-aji('Aji', 1994) = new Person;
-console.log(aji)
+// CONSRTUCTOR FUNCTION
+
+const Student = function(firstName, birthYear, course){
+    Person.call(this, firstName, birthYear);
+    this.course = course;
+}
+
+Student.prototype = Object.create(Person.prototype) // LINKING STUDENT TO PERSON (INHERITANCE)
+
+Student.prototype.introduce = function(){
+    console.log(`My name is ${this.firstName}, i was born in ${this.birthYear}, i am studying ${this.course}`);
+}
+
+const aji = new Student('Aji', 1994, 'Computer Science')
+
+const diana = new Student('Diana', 1997, 'English and Poetry')
+diana.introduce()
+aji.introduce()
+diana.calcAge()
